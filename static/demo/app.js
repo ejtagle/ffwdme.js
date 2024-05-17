747813ef0a3f8ba6526ffc1ff7ce55f60c26620b
ejtagle <ejtagle@hotmail.com>
2024-05-17 00:21:17 -0300

    Enter fullscreen on landscape mode

--- static/demo/app.js
+++ static/demo/app.js
@@ -1,36 +1,15 @@
 function init() {
 
-	/*! Normalized address bar hiding for iOS & Android (c) @scottjehl MIT License */
-	var win = window;
-	var doc = win.document;
-	
-	// If there's a hash, or addEventListener is undefined, stop here
-	if(!win.navigator.standalone && !location.hash){
-		
-		//scroll to 1
-		win.scrollTo( 0, 1 );
-		var scrollTop = 1,
-			getScrollTop = function(){
-				return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
-			},
-		
-			//reset to 0 on bodyready, if needed
-			bodycheck = setInterval(function(){
-				if( doc.body ){
-					clearInterval( bodycheck );
-					scrollTop = getScrollTop();
-					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-				}	
-			}, 15 );
-		
-		setTimeout(function(){
-			//at load, if user hasn't scrolled more than 20 or so...
-			if( getScrollTop() < 20 ){
-				//reset to hide addr bar at onload
-				win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-			}
-		}, 0);
-	}
+	window.screen.orientation.onchange = function() {
+		var el = document.documentElement;
+		var rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen ;
+		var efs = document.exitFullscreen || document.webkitExitFullscreen || document.mozExitFullscreen ;
+		if (this.type.startsWith('landscape')) {
+			rfs.call(el);
+		} else {
+			efs.call(document);
+		}
+	};
 
     ffwdme.on('geoposition:init', function () {
         console.info("Waiting for initial geoposition...");
